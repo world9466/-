a1a3a542155fd6b14aa512f1963f0ba5f0de908c
world9466 <101788002+world9466@users.noreply.github.com>
2023-05-08 14:15:24 +0800

    fix: 修正伺服器上傳沒有反應問題
    修改項目：
    1. 伺服器上傳NLP文字雲會有偶發性失敗，新增嘗試3次上傳動作。
    2. 即使失敗同樣會在公司LINE群通知網頁更新。
    
    Ver_1.1.1

--- keyword_auto/ftp_keyword.py
+++ keyword_auto/ftp_keyword.py
@@ -3,6 +3,7 @@ from datetime import datetime
 import paramiko
 
 
+
 client = paramiko.SSHClient()
 
 client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
@@ -11,44 +12,140 @@ client.connect(hostname='10.227.58.87', username='root', password='0266007766')
 t = client.get_transport()
 sftp=paramiko.SFTPClient.from_transport(t)
 
-# 查看目前目錄
-'''
-stdin, stdout, stderr = client.exec_command('cd /var/www/html;ls -al')
-result = stdout.readlines()
 
-for res in result:
-    print(res)
-'''
+# 嘗試上傳
+try:
+    # 查看目前目錄
+    '''
+    stdin, stdout, stderr = client.exec_command('cd /var/www/html;ls -al')
+    result = stdout.readlines()
 
-# 上傳檔案
+    for res in result:
+        print(res)
+    '''
 
-sftp.put("index.html", "/var/www/html/keyword/index.html")
-sftp.put("media.jpg", "/var/www/html/keyword/media.jpg")
-sftp.put("NLP/NLP-Cloud.png", "/var/www/html/keyword/NLP/NLP-Cloud.png")
-stdin, stdout, stderr = client.exec_command('cd /var/www/html/keyword;ls -al')
-result = stdout.readlines()
-for res in result:
-    print(res)
+    # 上傳檔案
+    sftp.put("index.html", "/var/www/html/keyword/index.html")
+    sftp.put("NLP/NLP-Cloud.png", "/var/www/html/keyword/NLP/NLP-Cloud.png")
+    stdin, stdout, stderr = client.exec_command('cd /var/www/html/keyword;ls -al')
+    result = stdout.readlines()
+    for res in result:
+        print(res)
 
-print('web-update successful')
+    print('web-update successful')
+
+    # 建立歷史網頁資料夾
+    # 因為後一時段的功能只有做在歷史頁面內，為了避錯，最新頁沒有加入，所以要從晨午晚報_資料/history資料夾裡面抓取檔案上傳
+    time_now = datetime.now().strftime('%Y-%m-%d_%H')
+
+    stdin, stdout, stderr = client.exec_command('cd /var/www/html/keyword/history;mkdir {}'.format(time_now))
+    stdin, stdout, stderr = client.exec_command('cd /var/www/html/keyword/history/{};mkdir NLP'.format(time_now))
+
+    result = stdout.readlines()
+    dirpath = '../../晨午晚報_資料/history/keyword'
+    files = os.listdir(dirpath)
+    for file in files:
+        if re.match('關鍵字 - {}'.format(time_now),file):
+            print('history_file detected')
+            sftp.put("../../晨午晚報_資料/history/keyword/"+file, "/var/www/html/keyword/history/{}/index.html".format(time_now))
+
+    # 把NLP圖片上傳到資料夾
+    sftp.put("NLP/NLP-Cloud.png", "/var/www/html/keyword/history/{}/NLP/NLP-Cloud.png".format(time_now))
+
+    print('web-history update successful')
+    client.close()
+
+except:
+    # 再試一次
+    print('wait 7 sec for re-uploading...')
+    time.sleep(7)
+    try:
+        # 上傳檔案
+        sftp.put("index.html", "/var/www/html/keyword/index.html")
+        sftp.put("NLP/NLP-Cloud.png", "/var/www/html/keyword/NLP/NLP-Cloud.png")
+        stdin, stdout, stderr = client.exec_command('cd /var/www/html/keyword;ls -al')
+        result = stdout.readlines()
+        for res in result:
+            print(res)
+
+        print('web-update successful')
+
+        # 建立歷史網頁資料夾
+        # 因為後一時段的功能只有做在歷史頁面內，為了避錯，最新頁沒有加入，所以要從晨午晚報_資料/history資料夾裡面抓取檔案上傳
+        time_now = datetime.now().strftime('%Y-%m-%d_%H')
+
+        stdin, stdout, stderr = client.exec_command('cd /var/www/html/keyword/history;mkdir {}'.format(time_now))
+        stdin, stdout, stderr = client.exec_command('cd /var/www/html/keyword/history/{};mkdir NLP'.format(time_now))
+
+        result = stdout.readlines()
+        dirpath = '../../晨午晚報_資料/history/keyword'
+        files = os.listdir(dirpath)
+        for file in files:
+            if re.match('關鍵字 - {}'.format(time_now),file):
+                print('history_file detected')
+                sftp.put("../../晨午晚報_資料/history/keyword/"+file, "/var/www/html/keyword/history/{}/index.html".format(time_now))
+
+        # 把NLP圖片上傳到資料夾
+        sftp.put("NLP/NLP-Cloud.png", "/var/www/html/keyword/history/{}/NLP/NLP-Cloud.png".format(time_now))
+
+        print('web-history update successful')
+        client.close()
+
+    except:
+        # 再試一次
+        print('wait 7 sec for re-uploading...')
+        time.sleep(7)
+        try:
+            # 上傳檔案
+            sftp.put("index.html", "/var/www/html/keyword/index.html")
+            sftp.put("NLP/NLP-Cloud.png", "/var/www/html/keyword/NLP/NLP-Cloud.png")
+            stdin, stdout, stderr = client.exec_command('cd /var/www/html/keyword;ls -al')
+            result = stdout.readlines()
+            for res in result:
+                print(res)
+
+            print('web-update successful')
+
+            # 建立歷史網頁資料夾
+            # 因為後一時段的功能只有做在歷史頁面內，為了避錯，最新頁沒有加入，所以要從晨午晚報_資料/history資料夾裡面抓取檔案上傳
+            time_now = datetime.now().strftime('%Y-%m-%d_%H')
+
+            stdin, stdout, stderr = client.exec_command('cd /var/www/html/keyword/history;mkdir {}'.format(time_now))
+            stdin, stdout, stderr = client.exec_command('cd /var/www/html/keyword/history/{};mkdir NLP'.format(time_now))
+
+            result = stdout.readlines()
+            dirpath = '../../晨午晚報_資料/history/keyword'
+            files = os.listdir(dirpath)
+            for file in files:
+                if re.match('關鍵字 - {}'.format(time_now),file):
+                    print('history_file detected')
+                    sftp.put("../../晨午晚報_資料/history/keyword/"+file, "/var/www/html/keyword/history/{}/index.html".format(time_now))
+
+            # 把NLP圖片上傳到資料夾
+            sftp.put("NLP/NLP-Cloud.png", "/var/www/html/keyword/history/{}/NLP/NLP-Cloud.png".format(time_now))
+
+            print('web-history update successful')
+            client.close()
+
+        except Exception as errormsg:
+            print('Cti server access false，stoping...')
+
+            # 設定 line 推播函式，再開頭就加入會報錯
+            import requests
+            def lineNotifyMessage(token, msg):
+                headers = {
+                    "Authorization": "Bearer " + token, 
+                    "Content-Type" : "application/x-www-form-urlencoded"
+                }
+
+                payload = {'message': msg }
+                r = requests.post("https://notify-api.line.me/api/notify", headers = headers, params = payload)
+                return r.status_code
+            token = 'm6uafWsyIziRWXaqYfTKJxNShGYlp3WM3RG9e0hP2OA'
 
 
-# 建立歷史網頁資料夾
-# 因為後一時段的功能只有做在歷史頁面內，為了避錯，最新頁沒有加入，所以要從history資料夾裡面抓取檔案上傳
-time_now = datetime.now().strftime('%Y-%m-%d_%H')
-
-stdin, stdout, stderr = client.exec_command('cd /var/www/html/keyword/history;mkdir {}'.format(time_now))
-stdin, stdout, stderr = client.exec_command('cd /var/www/html/keyword/history/{};mkdir NLP'.format(time_now))
-
-result = stdout.readlines()
-dirpath = '../../晨午晚報_資料/history/keyword'
-files = os.listdir(dirpath)
-for file in files:
-    if re.match('關鍵字 - {}'.format(time_now),file):
-        print('history_file detected')
-        sftp.put("../../晨午晚報_資料/history/keyword/"+file, "/var/www/html/keyword/history/{}/index.html".format(time_now))
-sftp.put("media.jpg", "/var/www/html/keyword/history/{}/media.jpg".format(time_now))
-sftp.put("NLP/NLP-Cloud.png", "/var/www/html/keyword/history/{}/NLP/NLP-Cloud.png".format(time_now))
-
-print('web-history update successful')
-client.close()
+            today = datetime.now().strftime('%Y-%m-%d %H時')
+            message = '公司伺服器上傳有誤，發生時間點：{}\n'.format(today)
+            lineNotifyMessage(token, message+str(errormsg))
+            
+            os._exit(0)
