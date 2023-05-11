04ea67cdb8bef65118bbd02178c7cddc4fbe87f0
world9466 <101788002+world9466@users.noreply.github.com>
2023-05-11 11:11:50 +0800

    feat: 新增NLP字詞庫於伺服器存取功能
    修改項目：
    1. 為方便同仁及時修改NLP字詞庫，每次執行程式都會先下載字詞庫，更新後再上傳。
    2. 只要直接在伺服器修改檔案就可以更新字詞庫。
    3. 遇到伺服器錯誤時自動中斷。
    
    Ver_1.1.2

--- keyword_auto/google_keyword.py
+++ keyword_auto/google_keyword.py
@@ -3,6 +3,7 @@ from bs4 import BeautifulSoup
 from datetime import datetime
 from jsonpath import jsonpath
 from fake_useragent import UserAgent
+import paramiko
 #pip install jieba
 #pip install BeautifulSoup4
 #pip install jsonpath
@@ -25,10 +26,40 @@ def lineNotifyMessage(token, msg):
 token = 'm6uafWsyIziRWXaqYfTKJxNShGYlp3WM3RG9e0hP2OA'
 
 
+#### 下載NLP字詞庫  ####
+
+# 建立連線
+client = paramiko.SSHClient()
+
+client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+
+client.connect(hostname='10.227.58.87', username='root', password='0266007766')
+t = client.get_transport()
+sftp=paramiko.SFTPClient.from_transport(t)
+
+try:
+    file_list = ['NLP-CN_words.txt','NLP-Event.txt','NLP-Org.txt','NLP-People.txt','NLP-Region.txt','NLP-stopwords.txt']
+    for file in file_list:
+        print('downloading {}'.format(file))
+        sftp.get('/var/www/html/keyword/NLP/{}'.format(file), 'NLP/{}'.format(file))
+
+except Exception as errormsg:
+    print('downloading failed...')
+    print(errormsg)
+    pass
+
+#### 下載NLP字詞庫-完成  ####
+
+
+
+
+#### 擷取google關鍵字  ####
+
 time_trend = datetime.now().strftime('%Y%m%d')
 time_trend = str(time_trend)
 
 # 嘗試編碼擷取資料，若出現錯誤就中斷執行
+
 try:
     # 修改網址為目前時間
     trends_url = 'https://trends.google.com.tw/trends/api/dailytrends?hl=zh-TW&tz=-480&ed={}&geo=TW&ns=15'.format(time_trend)
@@ -135,39 +166,83 @@ except Exception as errormsg:
 
 rw.close()
 
+
 ###寫入關鍵字至 txt - trends_title_list
+
+'''把 Google 資料拆分後 放入字典'''
+import jieba
 import jieba.posseg as pseg
 
+
 try:
-    for i in range(len(trends_articles_list)):
-
-        text = trends_articles_list[i]
+    for i in range(len(trends_title_list)): 
+        text = trends_title_list[i]
         words = pseg.cut(text)
-
+        print(f'text:{text}')
         for word, flag in words:
-            if flag == 'nr':
-                if len(word)>1:
-                    #print("人名：", word)
-                    with open('NLP/NLP-People.txt', 'a',encoding="utf-8") as f:
+            #基本字典
+            if len(word) > 1:
+                with open('NLP/NLP-CN_words.txt', 'a',encoding='utf-8') as f:
+                    f.write(word+"\n")
+
+            #個人字典
+            if flag == 'nr' or flag == 'nrfg':
+                if len(word) > 2:
+                    print("人名：", word)
+                    with open('NLP/NLP-People.txt', 'a',encoding='utf-8') as f:
                         f.write(word+"\n")
 
-            elif flag == 'ns':
-                if len(word)>1:
-                    #print("地名：", word)
-                    with open('NLP/NLP-Region.txt', 'a',encoding="utf-8") as f:
+            elif flag == 'ns' or flag == 'nrt':
+                if len(word) >1:
+                    print("地名：", word)
+                    with open('NLP/NLP-Region.txt', 'a',encoding='utf-8') as f:
                         f.write(word+"\n")
-            
+
             elif flag == 'nt':
-                if len(word)>1:
-                    #print("組織：", word)
-                    with open('NLP/NLP-Org.txt', 'a',encoding="utf-8") as f:
+                if len(word) >1:
+                    print("組織：", word)
+                    with open('NLP/NLP-Org.txt', 'a',encoding='utf-8') as f:
                         f.write(word+"\n")
-            
             else:
-                if len(word)>1:
-                    #print("其它：", word)
-                    with open('NLP/NLP-CN_words.txt', 'a',encoding="utf-8") as f:
+                if len(word) >1:
+                    print("事件：", word)
+                    with open('NLP/NLP-Event.txt', 'a',encoding='utf-8') as f:
                         f.write(word+"\n")
+    f.close()
+
+
+    ### 剔除已有單字
+    stopList = ["NLP/NLP_People.txt","NLP/NLP_Region.txt","NLP_Org"]
+    stop = [line.strip() for line in open("NLP/NLP-stopwords.txt",encoding="utf-8").readlines()]
+    with open('NLP/NLP-Event.txt', 'a',encoding='utf-8') as f:
+                        f.write(word+"\n")
+
+
+    ''' 重複內容過濾'''
+    StopList = ['NLP/NLP-Region.txt','NLP/NLP-Org.txt','NLP/NLP-People.txt']
+
+    with open('NLP/NLP-Event.txt', 'r', encoding='utf-8') as f1:
+        nlp_event = set(f1.read().splitlines())
+
+
+    for i in range(len(StopList)):
+        with open(StopList[i], 'r', encoding='utf-8') as f2:
+            nlp_stop = set(f2.read().splitlines())
+
+
+        overlap = nlp_event.intersection(nlp_stop)
+        if overlap:
+            print("有重複的內容：", overlap)
+            with open('NLP/NLP-Event.txt', 'r+', encoding='utf-8') as f5:
+                lines = f5.readlines()
+                f5.seek(0)
+                for line in lines:
+                    if line.strip() not in overlap:
+                        f5.write(line)
+                f5.truncate()
+            print("重複的內容已自動刪除")
+        else:
+            print("沒有重複的內容")
 
     ### 刪除重複字元
     def overwrite(txt):  
@@ -179,15 +254,29 @@ try:
 
     overwrite('NLP/NLP-People.txt')
     overwrite('NLP/NLP-Region.txt')
+    overwrite('NLP/NLP-Event.txt')
     overwrite('NLP/NLP-Org.txt')
     overwrite('NLP/NLP-CN_words.txt')
-    overwrite('NLP/NLP-stopwords.txt')
-    print("讀寫完成")
-
-except Exception as errormsg:
+    overwrite('NLP/NLP-stopwords.txt')    
+    
+except:
     print('寫入關鍵字失敗')
-    today = datetime.now().strftime('%Y-%m-%d %H時')
-    message = 'google資料獲取有誤，發生時間點：{}\n'.format(today)
-    lineNotifyMessage(token, message+str(errormsg))
     time.sleep(5)
     os._exit(0)
+
+
+
+#### 上傳更新的NLP字詞庫  ####
+
+try:
+    file_list = ['NLP-CN_words.txt','NLP-Event.txt','NLP-Org.txt','NLP-People.txt','NLP-Region.txt','NLP-stopwords.txt']
+    for file in file_list:
+        print('uploading {}'.format(file))
+        sftp.put('NLP/{}'.format(file) , '/var/www/html/keyword/NLP/{}'.format(file))
+
+except Exception as errormsg:
+    print('uploading failed...')
+    print(errormsg)
+    pass
+
+#### 上傳更新的NLP字詞庫-完成  ####
